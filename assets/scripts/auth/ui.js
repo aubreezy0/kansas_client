9c9c69f7bcc9399c4c1745235e21cbd42ca2d029
aubreezy0 <aubreezy0@gmail.com>
2019-06-03 18:14:29 -0400

    adds handlebar templates for refactor. adds api.js, events.js, ui.js files for auth. have not yet been tested.

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,120 @@
+'use strict'
+const store = require('../store')
+
+// const signUpTemplate = require('../templates/sign-up.handlebars')
+// const signInTemplate = require('../templates/sign-in.handlebars')
+// const changePasswordTemplate = require('../templates/change-password.handlebars')
+
+const signUpSuccess = function () {
+  // console.log('from signUpSuccess')
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed up</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 10000)
+  $('.sign-up').addClass('hidden')
+}
+
+const signUpFailure = function () {
+  $('.modal').modal('hide')
+  $('#message').html(`<div class="alert alert-danger" role="alert">OH NO! What did you do?! Clearly you did <em>something</em> wrong. Maybe you're already a user? Did your passwords match? Easy mistakes, we still like you. <br>Try again!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 10000)
+}
+
+const signInSuccess = function (data) {
+  // console.log(data)
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed in!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  $('.modal').modal('hide')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 10000)
+  $('.sign-up').addClass('hidden')
+  $('.sign-in').addClass('hidden')
+  $('.sign-out').removeClass('hidden')
+  $('.change-password').removeClass('hidden')
+
+  $('.lunch-stuff').removeClass('hidden')
+  store.user = data.user
+  // store.user = data.user
+  // const createFileUploadHandlebars = require('../templates/file-upload/create-file.handlebars')
+  // const createFileUploadHTML = createFileUploadHandlebars()
+}
+
+const signInFailure = function () {
+  $('.modal').modal('hide')
+  $('#message').html(`<div class="alert alert-danger" role="alert">Whoops! That didn't work. Please try to Sign In again, or if you aren't already a lucky member of the Magic Lunch Ball clique, go ahead and Sign Up!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 10000
+  )
+}
+
+const changePasswordSuccess = function () {
+  $('.modal').modal('hide')
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully changed your password!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 3000
+  )
+}
+
+const changePasswordFailure = function () {
+  $('.modal').modal('hide')
+  $('#message').html(`<div class="alert alert-danger" role="alert">Failed to change password</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 3000
+  )
+}
+
+const signOutSuccess = function (data) {
+  $('#message').html(`<div class="alert alert-success" role="alert">You have succesfully signed out!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  $('#show').html('')
+  $('#no-show').html('')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 3000
+  )
+  $('.sign-up').removeClass('hidden')
+  $('.sign-in').removeClass('hidden')
+  $('.sign-out').addClass('hidden')
+  $('.change-password').addClass('hidden')
+  $('.lunch-stuff').addClass('hidden')
+  store.user = null
+}
+
+const signOutFailure = function () {
+  $('#message').html(`<div class="alert alert-danger" role="alert">Failed to sign out!</div>`)
+  $('#message').css('text-align', 'center')
+  $('form').trigger('reset')
+  setTimeout(() => {
+    $('#message').html('')
+  }, 10000
+  )
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
